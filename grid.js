8483c88517d29999718696cc1cc202071e478e34
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-06-07 11:12:23 +0200

    Edit grid.js to add button to change grid dimensions, prompting user to enter new value and make a new grid based on userinput in the window. Also edited style.css to style button and remove fixed width and height of squares (this is now changed dynamically in gird.js

--- grid.js
+++ grid.js
@@ -1,27 +1,69 @@
 const container = document.querySelector("div");
 
-const GRID_DIMENSION = 16;
+let grid_dimension = 16;
+const WIN_X = 960;
+const WIN_Y = 960;
 
-for(let i=0;i<GRID_DIMENSION;i++){
-    //create new row
-    const row = document.createElement("div");
-    row.className='row';
-    for(let j=0;j<GRID_DIMENSION;j++){
-        const square = document.createElement("div");
-        square.className='square-div';
-        row.appendChild(square);
+//Layout grid
+function layOutGrid(grid_dimension){
+    //Subtracting borders to allow for taking up the same space
+    const sq_width = (WIN_X-grid_dimension)/grid_dimension;
+    const sq_height = (WIN_Y-grid_dimension)/grid_dimension;
+    for(let i=0;i<grid_dimension;i++){
+        //create new row
+        const row = document.createElement("div");
+        row.className='row';
+        for(let j=0;j<grid_dimension;j++){
+            const square = document.createElement("div");
+            square.className='square-div';
+            square.style.width = sq_width + "px";
+            square.style.height = sq_height + "px";
+            row.appendChild(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+    //Add eventhandler - mouseover changes class to change bg-color
+    const squares = document.querySelectorAll(".square-div");
+
+    squares.forEach(item => {
+        item.addEventListener('mouseover', (event) => {
+        item.className='hover';
+        });
+    });
 }
 
-const squares = document.querySelectorAll(".square-div");
+layOutGrid(grid_dimension);
 
-squares.forEach(item => {
-    item.addEventListener('mouseover', (event) => {
-       item.className='hover';
-   });
-});
 
+
+//Button top to change number of squares in grid
+const btn = document.createElement("button");
+btn.className='button';
+btn.textContent='Change grid-dimension';
+
+//Get rowdiv to insert button on top of page
+const row = document.querySelector('.row');
+container.insertBefore(btn,row);
+
+//Event-handler for button to change number of squares
+btn.addEventListener('click', (event) => {
+    let size;
+    do{
+    size = prompt('Number of squares per side? (max. 100)');
+    size = parseInt(size);
+    }while(size > 100 || size <= 0 || !Number.isInteger(size))
+    changeGrid(size);
+})
+
+
+function changeGrid(size) {
+    console.log(`You want this grid-dimension: ${size}. `);
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(item => {
+        container.removeChild(item);
+    })
+    layOutGrid(size);
+}
 
 
 
